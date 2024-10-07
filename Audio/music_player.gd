8d60db9943e1e667ba4875fb777e9254427d61ff
Doubi88 <tobis_mail@yahoo.de>
2024-10-07 22:37:43 +0200

    Added license and removed music

--- Audio/music_player.gd
+++ Audio/music_player.gd
@@ -1,12 +1,12 @@
 extends AudioStreamPlayer
 
 @onready var tracks: Array[AudioStream] = [
-	preload("res://Audio/just-relax-11157.mp3"),
-	preload("res://Audio/my-life-main-6670.mp3"),
-	preload("res://Audio/coniferous-forest-142569.mp3"),
-	preload("res://Audio/forest-lullaby-110624.mp3"),
-	preload("res://Audio/leva-eternity-149473.mp3"),
-	preload("res://Audio/summer-walk-152722.mp3")
+	#preload("res://Audio/Music/just-relax-11157.mp3"),
+	#preload("res://Audio/Music/my-life-main-6670.mp3"),
+	#preload("res://Audio/Music/coniferous-forest-142569.mp3"),
+	#preload("res://Audio/Music/forest-lullaby-110624.mp3"),
+	#preload("res://Audio/Music/leva-eternity-149473.mp3"),
+	#preload("res://Audio/Music/summer-walk-152722.mp3")
 ]
 
 var current := -1
@@ -17,9 +17,10 @@ func _ready() -> void:
 	play_next()
 
 func play_next() -> void:
-	current = (current + 1) % tracks.size()
-	stream = tracks[current]
-	play()
+	if (tracks.size() > 0):
+		current = (current + 1) % tracks.size()
+		stream = tracks[current]
+		play()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("next_track"):
