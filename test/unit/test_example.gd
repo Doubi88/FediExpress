dd5e52f877057e2b31784e74e337cec01726db05
André Jaenisch <andre.jaenisch@posteo.de>
2024-10-15 09:26:11 +0200

    test: prepare GUT
    GUT is a framework to allow for writing tests in Godot.
    Since it is installed by each Godot editor instance I'm merely tracking
    the presence of the folder here.
    
    Please see GUT Installation Guide for installation and setup.
    https://gut.readthedocs.io/en/latest/Quick-Start.html
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ test/unit/test_example.gd
@@ -0,0 +1,9 @@
+extends GutTest
+
+func test_passes():
+	# this test will pass because 1 does equal 1
+	assert_eq(1, 1)
+
+func test_fails():
+	# this test will fail because those strings are not equal
+	assert_eq('hello', 'goodbye')
